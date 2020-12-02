72eeccf050322c59e9eae145660d591425ffbf55
00008883 <wiut.00008883@gmail.com>
2020-12-02 14:26:59 +0500

    update

--- binary.py
+++ binary.py
@@ -1,4 +1,6 @@
-def binarySearchRecursive(sequence, x, left, right):
+def binarySearchRecursive(sequence, x):
+    left = 0
+    right = len(sequence)-1
     while left <= right:
         mid = (left + right) // 2
 
@@ -15,4 +17,4 @@ def binarySearchRecursive(sequence, x, left, right):
 if __name__ =="__main__":
     sample = [1, 2, 5, 6, 8, 10, 50, 70, 75, 77, 78, 79, 80, 81, 82, 85]
     x = 77
-    print('Index of number:', x, 'is equal to', binarySearchRecursive(sample, x, 0, len(sample)-1))
+    print('Index of number:', x, 'is equal to', binarySearchRecursive(sample, x))
